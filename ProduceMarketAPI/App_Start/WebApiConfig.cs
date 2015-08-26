ca008ee7831d3bcbeba44cd51efb1535d34a228e
grimcoder <papochka12>
2015-08-26 16:46:21 -0700

    enabled cors

--- ProduceMarketAPI/App_Start/WebApiConfig.cs
+++ ProduceMarketAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace ProduceMarketAPI
@@ -13,12 +14,14 @@ namespace ProduceMarketAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.EnableCors();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
--- ProduceMarketAPI/Controllers/PricesController.cs
+++ ProduceMarketAPI/Controllers/PricesController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Serialization;
 using System.Web.Http;
+using System.Web.Http.Cors;
+
 
 namespace ProduceMarketAPI.Controllers
 {
+    [DataContract]
+    public class PriceClass 
+    {
+        [DataMember]
+        public long Id { get; set; }
+        [DataMember]        
+        public double Price { get; set; }
+        [DataMember]        
+        public string ItemName { get; set; } 
+    }
+
     public class PricesController : ApiController
     {
-        public string[] GetAllPrices()
+
+        public static PriceClass[] prices =
         {
-            return new string[]{};
+            new PriceClass{Id = 7, ItemName = "Cabbage", Price = 4.5}, 
+            new PriceClass{Id = 8, ItemName = "Tomatos", Price = 3}, 
+            new PriceClass{Id = 9, ItemName = "Arguila", Price = 3.5}, 
+            new PriceClass{Id =10, ItemName = "Eggplant", Price = 2.4}, 
+            new PriceClass{Id = 6, ItemName = "Beet", Price = 4.5}
+
+        };
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public PriceClass[] GetAllPrices()
+        {
+            return prices;
         }
     }
 }
