67a54f38350cf9a7af5b943ab96d47e020d93ece
grimcoder <taraskovtun@gmail.com>
2015-08-26 18:05:43 -0700

    implemented json deserialization

--- ProduceMarketAPI/Controllers/PricesController.cs
+++ ProduceMarketAPI/Controllers/PricesController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using Newtonsoft.Json;
 
 
 namespace ProduceMarketAPI.Controllers
@@ -21,6 +23,8 @@ namespace ProduceMarketAPI.Controllers
         public string ItemName { get; set; } 
     }
 
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PricesController : ApiController
     {
 
@@ -34,7 +38,13 @@ namespace ProduceMarketAPI.Controllers
 
         };
 
-        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        static PricesController()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            string path = AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "\\prices.json";
+            prices = serializer.Deserialize<PriceClass[]>(new JsonTextReader(File.OpenText(path)));
+        }
+
         public PriceClass[] GetAllPrices()
         {
             return prices;
